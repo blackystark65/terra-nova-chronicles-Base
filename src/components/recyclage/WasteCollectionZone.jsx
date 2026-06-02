4b9cc97fe5f76a551eeedaaa81a25286a7ab8eb6
BOB Ecosphère du Rosey <blackystark65@gmail.com>
2026-06-02 18:29:41 +0000

    File changes

--- src/components/recyclage/WasteCollectionZone.jsx
+++ src/components/recyclage/WasteCollectionZone.jsx
@@ -243,7 +243,7 @@ export default function WasteCollectionZone({
                     whileHover={{ scale: 1.05 }}
                     className={`${bin.color} rounded-2xl p-4 text-white cursor-pointer`}
                     onClick={() => {
-                      if (selectedWaste && collectedWastes.includes(selectedWaste)) {
+                      if (selectedWaste) {
                         handleSortWaste(selectedWaste, key);
                       }
                     }}
